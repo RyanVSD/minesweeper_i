12fd3e915164cf124b85e294de14d0f25c7d19ac
Ryan Virtue <ryan.virtue@mrjaffesclass.com>
2023-05-10 12:44:44 -0700

    board made

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,45 +1,57 @@
 import React, { useEffect, useState } from 'react'; 
 import * as c from './Constants';
 import Square from './Square';
+import "./Board.css"
 
 export default function Board(props) {
     const [board, setBoard] = useState([]);
     useEffect(() => {
         let ar = [];
-        for(var i = 0; i < props.rows; i++) {
+        for(var i = 0; i < props.cols; i++) {
             let row = [];
             /*for(var j = 0; i < props.cols; j++) {
                row.push(c.CONSTANTS.Empty);
             }*/
             ar.push(row);
         }
-        for(var i = 0; i < props.rows; i++) {
-            for(var j = 0; j < props.cols; j++) {
+        for(i = 0; i < props.cols; i++) {
+            for(var j = 0; j < props.rows; j++) {
                 ar[i].push(c.CONSTANTS.Empty);
             }
         }
         setBoard(ar);
         console.log(ar);
     }, [props.cols, props.rows])
+
+
+    function getCols() {
+        return '1fr '.repeat(props.cols);
+    }
+
+    function getRows() {
+        return '1fr '.repeat(props.rows);
+    }
+
+    console.log(getCols());
+
     return (<div className="board">
         I am board with {props.cols} columns and
         {" "}{props.rows} rows with {props.mines} mines 
          {" "}resulting in a mine percent of 
          {" "}{props.mines / (props.rows * props.cols)}
-         <div>
+         <div style={{display: 'grid', gridTemplateColumns: getCols()}}>
          {board.map((row) => {
             console.log(row);
             return (
-             <div> 
+             <div style={{display: 'grid', gridTemplateRows: getRows()}}> 
                 {row.map((item) => {
-                    return <Square value={item}/>
+                    return <Square revealed={true} value={item}/>
                 })}
              </div>   
             )
             
         })}
          </div>
-        <Square value="0"/>
         </div>
     )
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,11 +1,19 @@
 import React from 'react'; 
 import { useState } from 'react';
+import './Square.css'
 
 export default function Square(props) {
     const [value, setValue] = useState(props.value);
-    return (
-        <div className="Square">
-            {value}
-        </div>
-    )
+    const [revealed, setRevealed] = useState(false);
+    if(revealed) {
+        return (
+            <button className="square-show" onClick={() => setRevealed(!revealed)}>
+                {value}
+            </button>
+        )
+    } else {
+        return <button className="square-hide" onClick={() => setRevealed(!revealed)}>
+            
+            </button>
+    }
 }
