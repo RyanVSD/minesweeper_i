1eb97dabd0afdd9c770995922134b40f68de4d73
Ryan Virtue <ryan.virtue@mrjaffesclass.com>
2023-05-10 13:07:58 -0700

    make generate mines now

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import Board from './Board';
 import {useState} from 'react';
 
 function App() {
-  const [rows, setRows] = useState(10);
-  const [cols, setCols] = useState(10);
-  const [mines, setMines] = useState(20);
+  const [rows, setRows] = useState(14);
+  const [cols, setCols] = useState(18);
+  const [mines, setMines] = useState(0.175);
 
   return (
     <div className="App">
-      <div>Minesweeper</div>
-      <Board rows={10} cols={10} mines={20}/>
+      <div style={{fontWeight: 'bold', margin: '10px'}}>Minesweeper</div>
+      <div>Board Sizes</div>
+      <button onClick={() => {setRows(8); setCols(10)}}>Small</button>
+      <button onClick={() => {setRows(14); setCols(18)}}>Med</button>
+      <button onClick={() => {setRows(20); setCols(24)}}>Large</button>
+      <br/> <br/>
+      <div>Mine Amount</div>
+      <button onClick={() => {setMines(0.1)}}>Easy</button>
+      <button onClick={() => {setMines(0.175)}}>Medium</button>
+      <button onClick={() => {setMines(0.25)}}>Hard</button>
+      <Board rows={rows} cols={cols} mines={Math.floor(rows*cols*mines)}/>
     </div>
   );
 }
--- src/Board.jsx
+++ src/Board.jsx
@@ -4,7 +4,11 @@ import Square from './Square';
 import "./Board.css"
 
 export default function Board(props) {
+    //Holds board data
     const [board, setBoard] = useState([]);
+    //Records whether the first mine is clicked
+    const [firstClick, setFirstClick] = useState(false);
+    //Inits empty board
     useEffect(() => {
         let ar = [];
         for(var i = 0; i < props.cols; i++) {
@@ -23,23 +27,21 @@ export default function Board(props) {
         console.log(ar);
     }, [props.cols, props.rows])
 
-
+    //Returns col number of 1frs
     function getCols() {
         return '1fr '.repeat(props.cols);
     }
-
+    //Returns row number of 1frs
     function getRows() {
         return '1fr '.repeat(props.rows);
     }
-
-    console.log(getCols());
-
+    
     return (<div className="board">
         I am board with {props.cols} columns and
         {" "}{props.rows} rows with {props.mines} mines 
          {" "}resulting in a mine percent of 
-         {" "}{props.mines / (props.rows * props.cols)}
-         <div style={{display: 'grid', gridTemplateColumns: getCols()}}>
+         {" "}{Math.round(props.mines / (props.rows * props.cols) * 1000)/10 + "%"}
+         <div className="main-board" style={{display: 'grid', gridTemplateColumns: getCols()}}>
          {board.map((row) => {
             console.log(row);
             return (
